1e80daa479569997066763f261f64dd8395b89b8
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-11-25 12:19:29 +0200

    Update server.c

--- PROIECT-SO2/server.c
+++ PROIECT-SO2/server.c
@@ -11,21 +11,14 @@
 char board[SIDE][SIDE]; // Tabla de joc
 
 // Inițializează tabla de joc cu spații libere
-void initializeBoard() 
-{
-    int i, j;
-    for (i = 0; i < SIDE; i++)
-    {
-        for (j = 0; j < SIDE; j++)
-        {
+void initializeBoard() {
+    for (int i = 0; i < SIDE; i++)
+        for (int j = 0; j < SIDE; j++)
             board[i][j] = ' ';
-        }
-    }
 }
 
 // Funcție pentru trimiterea tablei de joc către clienți
-void sendBoardToClients(int client1_fd, int client2_fd) 
-{
+void sendBoardToClients(int client1_fd, int client2_fd) {
     char boardStr[1024];
     snprintf(boardStr, sizeof(boardStr),
         "Tabla de joc:\n %c | %c | %c \n-----------\n %c | %c | %c \n-----------\n %c | %c | %c \n",
@@ -38,34 +31,22 @@ void sendBoardToClients(int client1_fd, int client2_fd)
 }
 
 // Funcție pentru verificarea câștigătorului
-int checkWinner() 
-{
-    int i;
-    for (i = 0; i < SIDE; i++) 
-    {
+int checkWinner() {
+    for (int i = 0; i < SIDE; i++) {
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
-        {
             return 1;
-        }
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
-        {
             return 1;
-        }
     }
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
-    {
         return 1;
-    }
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
-    {
         return 1;
-    }
     return 0;
 }
 
 // Verifică dacă tabla este completă (remiză)
-int checkDraw() 
-{
+int checkDraw() {
     for (int i = 0; i < SIDE; i++)
         for (int j = 0; j < SIDE; j++)
             if (board[i][j] == ' ')
@@ -73,15 +54,13 @@ int checkDraw()
     return 1; // Tabla e completă
 }
 
-int main() 
-{
+int main() {
     int server_fd, client1_fd, client2_fd;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
     // Creare socket server
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
@@ -91,13 +70,11 @@ int main()
     address.sin_port = htons(PORT);
 
     // Asociază socket-ul la port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) 
-    {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, MAX_CLIENTS) < 0) 
-    {
+    if (listen(server_fd, MAX_CLIENTS) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
@@ -105,15 +82,13 @@ int main()
     printf("Așteptând jucători...\n");
 
     // Acceptă conexiuni de la doi clienți
-    if ((client1_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) 
-    {
+    if ((client1_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
         perror("Accept client 1 failed");
         exit(EXIT_FAILURE);
     }
     printf("Jucătorul 1 conectat!\n");
 
-    if ((client2_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) 
-    {
+    if ((client2_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
         perror("Accept client 2 failed");
         exit(EXIT_FAILURE);
     }
@@ -123,8 +98,7 @@ int main()
     sendBoardToClients(client1_fd, client2_fd);  // Trimite tabla inițială ambilor clienți
 
     int currentPlayer = 0;
-    while (1) 
-    {
+    while (1) {
         int player_fd = currentPlayer == 0 ? client1_fd : client2_fd;
         char buffer[1024] = {0};
 
@@ -134,8 +108,7 @@ int main()
         int row = move / SIDE;
         int col = move % SIDE;
 
-        if (move < 0 || move >= SIDE * SIDE || board[row][col] != ' ') 
-        {
+        if (move < 0 || move >= SIDE * SIDE || board[row][col] != ' ') {
             char *invalidMoveMsg = "Mutare invalidă! Încercați din nou.\n";
             send(player_fd, invalidMoveMsg, strlen(invalidMoveMsg), 0);
             continue;
@@ -146,8 +119,7 @@ int main()
         sendBoardToClients(client1_fd, client2_fd);
 
         // Verifică starea jocului
-        if (checkWinner()) 
-        {
+        if (checkWinner()) {
             char *winMsg = "Ați câștigat!\n";
             char *loseMsg = "Ați pierdut!\n";
             send(player_fd, winMsg, strlen(winMsg), 0);
@@ -155,8 +127,7 @@ int main()
             break;
         }
 
-        if (checkDraw()) 
-        {
+        if (checkDraw()) {
             char *drawMsg = "Remiză!\n";
             send(client1_fd, drawMsg, strlen(drawMsg), 0);
             send(client2_fd, drawMsg, strlen(drawMsg), 0);
