3ac4c336961039f54e9400289b26db536f32d690
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-11-25 21:47:52 +0200

    Update client.c

--- PROIECT-SO2/client.c
+++ PROIECT-SO2/client.c
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#define PORT 8080
+
+
 // **Arhitectura**
 // Clientul este responsabil pentru:
 // - Conectarea la server.
@@ -17,8 +20,10 @@ void receiveAndShowBoard(int server_fd)
 {
     char boardStr[1024];
     int bytesReceived = recv(server_fd, boardStr, sizeof(boardStr) - 1, 0);
+    //recv() primește date de la server. Datele sunt stocate în boardStr
     if (bytesReceived > 0) 
     {
+        //Dacă se primesc date valide, acestea sunt afișate sub forma unei table de joc
         boardStr[bytesReceived] = '\0'; // Termină șirul primit
         printf("%s\n", boardStr); // Afișează tabla
     }
@@ -27,7 +32,8 @@ void receiveAndShowBoard(int server_fd)
 int main(void) 
 {
     int sock = 0;
-    struct sockaddr_in serv_addr;
+    struct sockaddr_in serv_addr;  //Adresa serverului la care clientul vrea să se conecteze trebuie 
+    //configurată. Acest lucru se face folosind o structură de tip sockaddr_in
 
     // **2. Crearea și conectarea la server**
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
@@ -37,17 +43,19 @@ int main(void)
     }
 
     // Configurarea adresei serverului
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_family = AF_INET;  //setează familia de adrese la IPv4
+    serv_addr.sin_port = htons(PORT); //setează portul la care clientul vrea să se conecteze (același port ca pe server: 8080)
 
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) 
     {
+        //convertește adresa IP a serverului din format text ("127.0.0.1") în format binar, necesar pentru conexiune
         printf("\nAdresă invalidă/Adresă nesuportată\n");
         return -1;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
     {
+        //inițiază o conexiune TCP către serverul specificat prin serv_addr
         printf("\nConexiune eșuată\n");
         return -1;
     }
@@ -62,11 +70,13 @@ int main(void)
         char buffer[1024] = {0};
         printf("Introduceți mutarea (1-9): ");
         fgets(buffer, sizeof(buffer), stdin);
+        // citește mutarea introdusă de utilizator din consolă
 
-        send(sock, buffer, strlen(buffer), 0); // Trimite mutarea la server
+        send(sock, buffer, strlen(buffer), 0); 
+        // trimite mutarea către server prin socketul sock
 
         char response[1024] = {0};
-        int bytesReceived = recv(sock, response, sizeof(response) - 1, 0);
+        int bytesReceived = recv(sock, response, sizeof(response) - 1, 0); //primește răspunsul de la server
         if (bytesReceived > 0) 
         {
             response[bytesReceived] = '\0';
@@ -79,6 +89,7 @@ int main(void)
         }
     }
 
+    //Când jocul se termină (din cauza unei victorii, înfrângeri sau remize), clientul închide conexiunea
     close(sock);
     return 0;
 }
