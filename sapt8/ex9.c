c08000b6d76f4354d7d933117b9dc4bbd596e7c0
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-12-08 14:53:44 +0200

    Update ex9.c

--- sapt8/ex9.c
+++ sapt8/ex9.c
@@ -86,3 +86,109 @@ int main(void)
     printf("\n\nTotal sum: %d\n", totalSum);
     return 0;
 }
+
+
+
+
+//sauuuu
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+
+#define N 2
+#define vectorSize 17
+
+int partialSum[N];
+int a[vectorSize];
+
+// Struct pentru a stoca intervalele și identificatorul thread-ului
+typedef struct {
+    int start;
+    int end;
+    int threadID;
+} Interval;
+
+void *threadFunction(void *arg)
+{
+    Interval *data = (Interval *)arg;
+
+    int start = data->start;
+    int end = data->end;
+    int threadID = data->threadID;
+
+    partialSum[threadID] = 0;
+
+    // Mesaj pentru thread
+    printf("\nThread %d is processing indices [%d, %d):\n", threadID, start, end);
+    int i;
+    for (i = start; i < end; i++) 
+    {
+        partialSum[threadID] += a[i];
+        printf("%d ", i);
+    }
+
+    free(data); // Eliberăm memoria alocată pentru struct
+    return NULL;
+}
+
+int main(void)
+{
+    pthread_t threads[N];
+    int i;
+
+    // Inițializare vector:
+    for (i = 0; i < vectorSize; i++) 
+    {
+        a[i] = i + 1;
+    }
+
+    int segmentSize = vectorSize / N;
+    int rest = vectorSize % N;
+
+    for (i = 0; i < N; i++)
+    {
+        // Alocare dinamică pentru fiecare structură ThreadData
+        Interval *data = malloc(sizeof(Interval));
+        if (data == NULL)
+        {
+            printf("Eroare la alocarea dinamică!!");
+            exit(1);
+        }
+
+        // Setăm limitele intervalului și ID-ul thread-ului
+        data->start = i * segmentSize;
+        if (i == N - 1) 
+        {
+            data->end = data->start + segmentSize + rest;
+        } 
+        else 
+        {
+            data->end = data->start + segmentSize;
+        }
+        data->threadID = i;
+        
+
+        if (pthread_create(&threads[i], NULL, threadFunction, data) != 0)
+        {
+            printf("Eroare la crearea thread-urilor!!");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Aștept toate thread-urile să se termine
+    for (i = 0; i < N; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    int totalSum = 0;
+    for (i = 0; i < N; i++)
+    {
+        totalSum += partialSum[i];
+    }
+
+    printf("\n\nTotal sum: %d\n", totalSum);
+    return 0;
+}
