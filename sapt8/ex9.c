3345a76ab666ffaa1a7177c3c00f315fd4783cee
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-12-08 14:17:35 +0200

    Update ex9.c

--- sapt8/ex9.c
+++ sapt8/ex9.c
@@ -1,8 +1,3 @@
-/*
-  Împarte un vector în segmente egale, fiecare fir calculează suma segmentului său, iar programul 
-principal calculează suma totală.
-*/
-
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -10,11 +5,10 @@ principal calculează suma totală.
 #include<unistd.h>
 
 #define N 2
-#define vectorSize 18
+#define vectorSize 17
 
 int partialSum[N];
 int a[vectorSize];
-
 
 void *threadFunction(void *arg)
 {
@@ -22,13 +16,21 @@ void *threadFunction(void *arg)
     free(arg);
 
     int start, end, i;
-    start = data * (vectorSize/N);
-    end = start + (vectorSize/N);
+    int rest = vectorSize % N;
+
+    start = data * (vectorSize / N);
+    end = start + (vectorSize / N);
+
+    // Ultimul thread preia și restul elementelor
+    if (data == N - 1) 
+    {
+        end = end + rest;
+    }
 
     partialSum[data] = 0;
-    for(i=start; i<end; i++)
+    for (i = start; i < end; i++) 
     {
-        partialSum[data] = partialSum[data] + a[i];
+        partialSum[data] += a[i];
     }
 
     return NULL;
@@ -39,43 +41,41 @@ int main(void)
     pthread_t threads[N];
     int i;
 
-    //initializare vector:
-    for(i=0; i<vectorSize; i++)
+    // Inițializare vector:
+    for (i = 0; i < vectorSize; i++) 
     {
-        a[i] = i+1;
+        a[i] = i + 1;
     }
     
-    for(i=0; i<N; i++)
+    for (i = 0; i < N; i++)
     {
-        int *threadID = malloc(sizeof(int));  //imi aloc dinamic spatiu pentru fiecare identificator
-        //am nevoie sa fac asta pentru a putea da ca parametru ulterior in pthread_create adresa unui astfel de element
-        //daca nu vreau sa aloc dinamic, pot creea in main un tablou de tip int threadIds[N] 
-        if(threadID == NULL)
+        int *threadID = malloc(sizeof(int));  // Alocare dinamică pentru fiecare identificator
+        if (threadID == NULL)
         {
-            printf("Eroare la alocarea dinamica!!");
+            printf("Eroare la alocarea dinamică!!");
             exit(1);
         }
 
         *threadID = i; // Stocăm valoarea identificatorului
-        if(pthread_create(&threads[i], NULL, threadFunction, threadID) != 0)
+        if (pthread_create(&threads[i], NULL, threadFunction, threadID) != 0)
         {
-            printf("Eroare la creearea thread-urilor!!");
+            printf("Eroare la crearea thread-urilor!!");
             exit(EXIT_FAILURE);
         }
     }
 
-    //astept toate thread-urile sa se termine
-    for(i=0; i<N; i++)
+    // Aștept toate thread-urile să se termine
+    for (i = 0; i < N; i++)
     {
         pthread_join(threads[i], NULL);
     }
 
     int totalSum = 0;
-    for(i=0; i<N; i++)
+    for (i = 0; i < N; i++)
     {
-        totalSum = totalSum + partialSum[i];
+        totalSum += partialSum[i];
     }
 
-    printf("Total sum: %d", totalSum);
+    printf("Total sum: %d\n", totalSum);
     return 0;
 }
